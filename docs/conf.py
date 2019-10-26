241951f9020965fe6e6b9a0b0749aa236ecb7b9a
Gaetano Bruno <info@gaetanobruno.eu>
2019-10-26 17:29:42 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ __version__ = '1.0'
 
 source_suffix = '.rst'
 master_doc = 'index'
-project = 'Gaetano-Bruno'
+project = 'Gaetano Bruno'
 copyright = '2019, libera'
 
 # The name of the Pygments (syntax highlighting) style to use.
